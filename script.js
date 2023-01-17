fd32f67c65787855e90baa7df2ab9dd0f09b0136
Raphael Oliveira <109704137+raphaaelj@users.noreply.github.com>
2023-01-17 17:26:20 -0300

    Patch 1.0
    Correções:
    -Alguns apps não abriam corretamente seus respectivos links;
    -Bordas, margens e espaçamentos.
    
    Adicionado:
    -App "Sobre mim";
    -App "Github";
    -App "Projetos".
    
    Removidos:
    -App "Facebook";
    -App "Instagram".

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-//Watch
+//Watch Start
 function watchTime() {
     var today = new Date();
     var h = today.getHours();
@@ -18,19 +18,21 @@ function checkTime(i) {
 }
 watchTime();
 checkTime();
-//End
+//Watch End
 
 
-//VAR APPS
+//Var APPS
 var mainScreen = document.getElementById("firstScreen"); //Main Screen
 var notificationBar = document.getElementById("notBar"); //Notification Bar
 var gmailApp = document.getElementById("gmailScreen"); //Gmail Screen
 var settingsApp = document.getElementById("settingsScreen"); //Settings Screen
 var bgChange = document.getElementById("bgChangeScreen"); //Settings Background Change Screen
 var bgImg = document.getElementsByClassName("background"); //Background change function
+var projectsApp = document.getElementById("projectsScreen"); //Projects Screen
+var aboutMeApp = document.getElementById("aboutMeScreen"); //About Me Screen
 
 
-//App GMAIL OPEN
+//Gmail OPEN
 function openGmail() {
     gmailApp.style.display = "flex";
     mainScreen.style.display = "none";
@@ -38,78 +40,94 @@ function openGmail() {
 }
 function sendMail() {
     alert('Email enviado! Obrigado pelo contato, retornarei o mais rápido possivel.');
+
 }
-//GMAIL END
+//Gmail END
+
+/* //AboutMe OPEN
+function openAboutMe() {
+    aboutMeApp.style.display = "flex";
+    mainScreen.style.display = "none";
+}
+//AboutMe END */
+
+//Projects OPEN
+function openProjects() {
+    projectsApp.style.display = "flex";
+    mainScreen.style.display = "none";
+}
 
 //Settings OPEN
 function openSettings() {
     mainScreen.style.display = "none";
     settingsApp.style.display = "flex";
+
 }
 function openBgChange() {
     settingsApp.style.display = "none";
     bgChange.style.display = "flex";
+
 }
 function closeBgChange() {
     bgChange.style.display = "none";
     settingsApp.style.display = "flex";
+
 }
 function bgOne() {
-    for(var i = 0; i < bgImg.length; i++) {
+    for (var i = 0; i < bgImg.length; i++) {
         bgImg[i].style.background = 'url(' + "images/bg-cellphone-1.jpg" + ')';
         bgImg[i].style.backgroundSize = "cover";
         bgImg[i].style.backgroundPosition = "center";
         bgImg[i].style.backgroundRepeat = "no-repeat";
     }
+
 }
 function bgTwo() {
-    for(var i = 0; i < bgImg.length; i++) {
+    for (var i = 0; i < bgImg.length; i++) {
         bgImg[i].style.background = 'url(' + "images/bg-cellphone-2.jpg" + ')';
         bgImg[i].style.backgroundSize = "cover";
         bgImg[i].style.backgroundPosition = "center";
         bgImg[i].style.backgroundRepeat = "no-repeat";
     }
+
 }
 function bgThree() {
-    for(var i = 0; i < bgImg.length; i++) {
+    for (var i = 0; i < bgImg.length; i++) {
         bgImg[i].style.background = 'url(' + "images/bg-cellphone-3.jpg" + ')';
         bgImg[i].style.backgroundSize = "cover";
         bgImg[i].style.backgroundPosition = "center";
         bgImg[i].style.backgroundRepeat = "no-repeat";
     }
+
 }
 function bgFour() {
-    for(var i = 0; i < bgImg.length; i++) {
+    for (var i = 0; i < bgImg.length; i++) {
         bgImg[i].style.background = 'url(' + "images/bg-cellphone-4.jpg" + ')';
         bgImg[i].style.backgroundSize = "cover";
         bgImg[i].style.backgroundPosition = "center";
         bgImg[i].style.backgroundRepeat = "no-repeat";
     }
+
 }
 function bgFive() {
-    for(var i = 0; i < bgImg.length; i++) {
+    for (var i = 0; i < bgImg.length; i++) {
         bgImg[i].style.background = 'url(' + "images/bg-cellphone-5.jpg" + ')';
         bgImg[i].style.backgroundSize = "cover";
         bgImg[i].style.backgroundPosition = "center";
         bgImg[i].style.backgroundRepeat = "no-repeat";
     }
+
 }
+//Settings END
 
 
+//Mobile BUTTON
+function backFirstScreen() {
+    gmailApp.style.display = "none";
+    notificationBar.style.background = "#191919"; //Cause GMAIL changes the color
+    settingsApp.style.display = "none";
+    bgChange.style.display = "none"; //Subscreen from settings
+    projectsApp.style.display = "none";
 
-//CELLPOHNE BUTTON CLOSE APPS AND SHOW FIRST SCREEN
-function backFirstScreen() { 
-    //GMAIL
-    if (gmailApp.style.display == "flex") {
-        gmailApp.style.display = "none";
-        mainScreen.style.display = "flex";
-        notificationBar.style.background = "#191919";
-    } //END
-
-    //SETTINGS
-    else if (settingsApp.style.display || bgChange.style.display == "flex") {
-        settingsApp.style.display = "none";
-        bgChange.style.display = "none";
-        mainScreen.style.display = "flex";
-    } //END
+    mainScreen.style.display = "flex";
 }
